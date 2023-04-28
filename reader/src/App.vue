2c22c5dd310bd65642ab9854c444e8fa160a047f
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-04-28 22:27:33 +0900

    QRコードを読み取る処理を実装

--- reader/src/App.vue
+++ reader/src/App.vue
@@ -3,6 +3,7 @@ import { ref, onMounted } from "vue";
 import JSQR from "jsqr";
 
 const videoElement = ref<HTMLVideoElement | null>(null);
+const canvasElement = ref<HTMLCanvasElement | null>(null);
 
 /**
  * カメラ起動
@@ -10,7 +11,9 @@ const videoElement = ref<HTMLVideoElement | null>(null);
 const startCamera = async () => {
   try {
     if (videoElement.value) {
-      const stream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } });
+      const stream = await navigator.mediaDevices.getUserMedia({
+        video: { facingMode: "environment" },
+      });
       videoElement.value.srcObject = stream;
       videoElement.value.play();
     }
@@ -24,18 +27,36 @@ const startCamera = async () => {
  * カメラのキャプチャ
  */
 const capture = () => {
+  if (!videoElement.value || !canvasElement.value) { return; }
+
+  const ctx = canvasElement.value.getContext("2d");
+  if (!ctx) { return; }
+
+  canvasElement.value.width = videoElement.value.videoWidth;
+  canvasElement.value.height = videoElement.value.videoHeight;
+  ctx.drawImage(videoElement.value, 0, 0, videoElement.value.videoWidth, videoElement.value.videoHeight);
+
+  const imageData = ctx.getImageData(0, 0, videoElement.value.videoWidth, videoElement.value.videoHeight);
+  const code = JSQR(imageData.data, imageData.width, imageData.height, { inversionAttempts: 'dontInvert' });
+
+  if (code) {
+    const result = JSON.parse(code.data);
+    console.log(result);
+    return;
+  }
+
   requestAnimationFrame(capture);
 };
 
 onMounted(() => {
   startCamera();
-})
-
+});
 </script>
 
 <template>
   <div class="root">
-    <video ref="videoElement" @play="capture()" />
+    <video ref="videoElement" @canplay="capture()" style="display: none" />
+    <canvas ref="canvasElement" style="display: none"></canvas>
   </div>
 </template>
 
