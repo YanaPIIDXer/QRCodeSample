a4136ef69a4991ec8e108f2f89637ca5932fd461
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-04-28 21:03:02 +0900

    QRコード生成処理

--- generate/src/App.vue
+++ generate/src/App.vue
@@ -1,7 +1,29 @@
 <script setup lang="ts">
+import { ref, onMounted } from "vue";
+import QrCode from "qrcode";
+
+const qrCode = ref("");
+const baseData = {
+  id: 1,
+  name: "Name",
+};
+
+onMounted(async () => {
+  try {
+    qrCode.value = await QrCode.toDataURL(JSON.stringify(baseData));
+  } catch (error) {
+    alert("エラー");
+    console.error(error);
+  }
+});
+
 </script>
 
 <template>
+  <div class="root">
+    <img v-if="qrCode" :src="qrCode" />
+    <span v-else>読み込み中</span>
+  </div>
 </template>
 
 <style lang="sass" scoped>
