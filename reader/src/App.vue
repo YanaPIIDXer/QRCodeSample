d10130dc20798eaeccdec949e1cd4e68b4f17d74
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-04-28 22:19:58 +0900

    カメラのキャプチャ

--- reader/src/App.vue
+++ reader/src/App.vue
@@ -1,7 +1,42 @@
 <script setup lang="ts">
+import { ref, onMounted } from "vue";
+import JSQR from "jsqr";
+
+const videoElement = ref<HTMLVideoElement | null>(null);
+
+/**
+ * カメラ起動
+ */
+const startCamera = async () => {
+  try {
+    if (videoElement.value) {
+      const stream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } });
+      videoElement.value.srcObject = stream;
+      videoElement.value.play();
+    }
+  } catch (error) {
+    alert("エラー");
+    console.error(error);
+  }
+};
+
+/**
+ * カメラのキャプチャ
+ */
+const capture = () => {
+  requestAnimationFrame(capture);
+};
+
+onMounted(() => {
+  startCamera();
+})
+
 </script>
 
 <template>
+  <div class="root">
+    <video ref="videoElement" @play="capture()" />
+  </div>
 </template>
 
 <style lang="sass" scoped>
